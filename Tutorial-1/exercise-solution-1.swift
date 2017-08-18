af8434a2e55e6e9659d82879c63b2a5b95577bc0
Priyanka Pakhale <ppakhale@asu.edu>
2017-08-18 08:35:48 +0530

    Create exercise-solution-1.swift
    Added exercise 1 solution

---
+++ Tutorial-1/exercise-solution-1.swift
@@ -0,0 +1,17 @@
+
+import Cocoa
+
+/* Exercise :
+ Create 3 variables. Store your first name in 1 variable, last name in second
+ variable, your age in 3rd variable.
+ 
+ Print this : "My name is Tech Academy. I am 24 years old."
+ Insert your first name + last name in place of Tech Academy and your age instead of 24.
+ 
+ */
+
+var first_name = "Priyanka"
+var last_name = "Pakhale"
+var age = 24
+
+print("My name is \(first_name) \(last_name). I am \(age) years old.")
